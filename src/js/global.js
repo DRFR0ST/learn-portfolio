ceaafcb512bf03d6ce4375f784450cb0ec49a69c
Mike Eling <mike.eling97@gmail.com>
2018-04-09 12:55:25 +0200

    Libs updated, files nested, meta added, general cleaning and improvement

---
+++ src/js/global.js
@@ -0,0 +1,11 @@
+/* Global Javascript file which contains all the stuff that the website is depending on */
+
+
+
+/* Other classes */
+
+class Czarnuch {
+	say(text) {
+		alert("Czarnuch mówi: " + text);
+	}
+}
